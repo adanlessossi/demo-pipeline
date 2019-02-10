8396ffc3740142a66f091a0c5bf9b78d4cdbcc11
adanlessossi <adanlessossi@hotmail.com>
2019-02-10 23:33:54 +0100

    Changed the timezone

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 exports.handler = (event, context, callback) => {
     var currentTime = ( new Date() ).toJSON().slice( 0, 19 ).replace( /[-T]/g, ':' );
-    console.log( ( currentTime.toString() ) );
+    console.log('Time is: ==> ', currentTime.toString() );
     callback( null, {
         statusCode: '200',
         body: 'The time in Zurich is: ' + currentTime.toString()
