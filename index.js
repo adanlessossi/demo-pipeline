a4779401f2dada097db9c2eb40455d0509f3c1cc
adanlessossi <adanlessossi@hotmail.com>
2019-02-10 22:14:56 +0100

    Changed the timezone

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var time = require( 'time' );
 
 exports.handler = ( event, context, callback ) => {
     var currentTime = new time.Date();
-    currentTime.setTimezone( "Amsterdam/Berlin" );
+    currentTime.setTimezone( "Europe/Zurich" );
     console.log( ( currentTime.toString() ) );
     callback( null, {
         statusCode: '200',
