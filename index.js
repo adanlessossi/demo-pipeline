f16c7a942907ac99958ec7045e2aed1cef9caf62
adanlessossi <adanlessossi@hotmail.com>
2019-02-10 10:53:03 +0100

    Trigger 1

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ exports.handler = ( event, context, callback ) =>
 {
     var currentTime = new time.Date();
     currentTime.setTimezone( "Amsterdam/Berlin" );
+    console.log( ( currentTime.toString() ) );
     callback( null, {
         statusCode: '200',
         body: 'The time in Zurich is: ' + currentTime.toString(),
