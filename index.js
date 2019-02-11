795cc055f18778f84bbf81b3965738d0a2fc7841
adanlessossi <adanlessossi@hotmail.com>
2019-02-11 08:34:46 +0100

    Changed the timezone

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
-exports.handler = (event, context, callback) => {
-    
+exports.handler = (event, context, callback) => {    
     var currentTime = ( new Date() ).toJSON().slice( 0, 19 ).replace( /[-T]/g, ':' );
     console.log('Time is: ==> ', currentTime.toString() );
     callback( null, {
